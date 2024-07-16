738cf1e31af7314b5ef4e1ba1c5fa685eb89bd26
unknown <nguyenhuuchau10@gmail.com>
2024-07-16 14:44:09 +0700

    adjust with env var from api_key warning

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import { useState, useEffect } from 'react';
 import Header from "./components/header/header.component"
 import Main from "./components/main/main.component"
 
-
 function App() {
   const [weatherData, setweatherData] = useState([])
-  // const api_key = "191b6752dc4cb3908988a414839dc067"
+  const api_Key = process.env.REACT_APP_API_KEY
   const [cityName, setcityName] = useState('Tây Ninh') 
   useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},vn&APPID=${api_key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},vn&APPID=${api_Key}`)
     .then((data) => data.json())
     .then((jsonData) => setweatherData(jsonData))
-  })
+  }, {weatherData})
 
   const cityHandler = (city) => {
     setcityName(city.target.value)
   }
 
+  console.log(weatherData.coord.lat)
+ 
   return (
     <div>
       <Header oncityHandler={cityHandler}/>
